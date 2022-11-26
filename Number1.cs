70137a45f854c7a79b0545f27f8be5a84fe9a1d4
Stephen Perez <64385314+Stphnprz09@users.noreply.github.com>
2022-11-26 09:56:42 +0800

    Add files via upload

---
+++ Number1.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment1;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string timeIn, timeOut;
+
+        Console.WriteLine("Time in: ");
+        timeIn = Console.ReadLine();
+        Console.WriteLine("Time out: ");
+        timeOut = Console.ReadLine();
+
+        TimeSpan WorkedHrs = DateTime.Parse(timeOut).Subtract(DateTime.Parse(timeIn));
+
+        Console.WriteLine("Time in: " + timeIn);
+        Console.WriteLine("Time out: " + timeOut);
+        Console.WriteLine("Worked Hours: " + WorkedHrs);
+
+    }
+}
+
